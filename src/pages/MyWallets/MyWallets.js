04ac85d6a919f89b9b6146acf711a183a62bd92e
Pham-Men <phamthimen0102@gmail.com>
2024-04-24 01:26:34 +0700

    hoan thanh giao dien my-wallet 24/ 4/ 2024

--- src/pages/MyWallets/MyWallets.js
+++ src/pages/MyWallets/MyWallets.js
@@ -1,7 +1,49 @@
-function MyWallets () {
+import Breadcrumbs from '@mui/material/Breadcrumbs';
+import Box from "@mui/material/Box";
+import Typography from '@mui/material/Typography';
+
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+
+import { bgGray } from "../../const/constCSS";
+import Wallet from '../../components/Wallet/Wallet';
+
+function MyWallets() {
     return (
         <>
-        MyWallets
+            <Box
+                sx={{
+                    backgroundColor: `${bgGray}`,
+                    height: '100vh'
+                }}
+            >
+                <Breadcrumbs
+                    sx={{
+                        height: '62px',
+                        backgroundColor: 'white',
+                        display: 'flex',
+                        alignContent: 'center'
+                    }}
+                >
+                    <ArrowBackIcon 
+                        sx={{
+                            marginLeft: '80px',
+                            marginRight: '36px'
+                        }}
+                    />
+                    <Typography
+                        sx={{
+                            fontFamily: 'Roboto, sans-serif',
+                            fontSize: '20px',
+                            fontWeight: '500',
+                            color: 'black',
+                            style: 'normal'
+                        }}
+                    >
+                        My Wallets
+                    </Typography>
+                </Breadcrumbs>
+                <Wallet/>
+            </Box>
         </>
     )
 }
