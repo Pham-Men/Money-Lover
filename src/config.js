9d954e631ceaeaf7f352fa39b27085b3b1660190
Pham-Men <phamthimen0102@gmail.com>
2024-04-24 12:23:29 +0700

    fix giao dien wallet 24/ 4/ 2024

--- src/config.js
+++ src/config.js
@@ -1,8 +1,9 @@
 import { getAuth, GoogleAuthProvider, OAuthProvider } from "firebase/auth";
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
+import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
+export const firebaseConfig = {
     apiKey: "AIzaSyBu0hSe7jvga1M4ZM1xOOUhZFAq2cM783Y",
     authDomain: "money-lover-8ed64.firebaseapp.com",
     projectId: "money-lover-8ed64",
@@ -15,9 +16,10 @@ const firebaseConfig = {
   const app = initializeApp(firebaseConfig);
   const auth = getAuth(app);
   const database = getDatabase(app);
+  const db = getFirestore(app);
   const provider = new GoogleAuthProvider();
   const providerApple = new OAuthProvider("apple.com");
 
 
-  export { auth, database, provider, providerApple};
+  export { db, auth, database, provider, providerApple};
  
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
 import sidebarReducer from './slices/sidebarSlice';
+import dataUserReducer from './slices/dataUserSlice';
 
 const store = configureStore({
     reducer: {
         auth: authReducer,
         sidebar: sidebarReducer,
+        dataUser: dataUserReducer,
     },
 })
 
